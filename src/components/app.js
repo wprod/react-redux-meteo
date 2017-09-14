f4d917e4a038cbe2cfa047a16d4b0148354a4b44
witters <witters@novius.fr>
2017-09-14 13:59:10 +0200

    working google maps :)

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="jumbotron">
+        <h1>Reac/Redux/Openweathermap</h1>
         <SearchBar/>
         <WeatherList/>
       </div>
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,47 +1,57 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { weather } from '../actions/index';
-import Chart from '../components/chart';
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {weather} from "../actions/index";
+import Chart from "../components/chart";
+import GoogleMap from "../components/google_map";
 
 class WeatherList extends Component {
-  renderWeather(cityData){
-    const name = cityData.city.name;
-    const data = cityData.list.map( (weather) => weather.main);
-    console.log(data);
-      return (
-        <tr key={name}>
-          <td>{name}</td>
-          <td>
-            <Chart data={data} dataKey={'temp'} fillColor={'orange'}/>
-          </td>
-          <td>
-            <Chart data={data} dataKey={'pressure'} fillColor={'red'}/>
-          </td>
-          <td>
-            <Chart data={data} dataKey={'humidity'} fillColor={'green'}/>
-          </td>
-        </tr>
-      )
-  };
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const data = cityData.list.map((weather) => weather.main);
 
-  render() {
-    return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
-    );
-  }
+        // const lon = cityData.city.coord.lon;
+        // const lat = cityData.city.coord.lat;
+
+        const {lat, lon} = cityData.city.coord; // ES6 <3
+        console.log(lat);
+        console.log(lon);
+
+
+        return (
+            <tr key={name}>
+                <td>
+                    <GoogleMap lon={lon} lat={lat}/>
+                </td>
+                <td>
+                    <Chart data={data} dataKey={'temp'} fillColor={'orange'}/>
+                </td>
+                <td>
+                    <Chart data={data} dataKey={'pressure'} fillColor={'red'}/>
+                </td>
+                <td>
+                    <Chart data={data} dataKey={'humidity'} fillColor={'green'}/>
+                </td>
+            </tr>
+        )
+    };
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                    <th>City</th>
+                    <th>Temperature</th>
+                    <th>Pressure</th>
+                    <th>Humidity</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    }
 }
 
 function mapStateToProps({weather}) {
